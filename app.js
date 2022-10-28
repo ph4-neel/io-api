afba00c5df21fab1bbfc9c3056c9c4437ee7dd64
indranil Bakshi <iamneel67@gmail.com>
2022-10-28 20:44:16 +0530

    neel changes

--- app.js
+++ app.js
@@ -2,60 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 require('./db/config');
-const User = require("./models/user");
+const userController = require('./controllers/userController');
+
 
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const router =new express.Router();
 
-
-
-
-
-app.get('/', (req, res) => {
-
-    res.send("Api's Are Working");
-
-});
-
-app.post('/saveUser', async (req, res) => {
-
-    try {
-
-        const user = new User(req.body);
-        const createUser = await user.save();
-        res.status(201).send(createUser);
-        console.log(createUser);
-
-
-    } catch (e) {
-
-        res.status(400).send("error is occuring " + e);
-
-    }
-});
-
-
-app.get('/getUser', async(req,res)=>{
-
-    try{
-
-        const data = await User.find();
-        res.status(201).send(data);
-
-    }catch(e){
-
-        res.status(400).send("error" + e);
-
-    }
-
-});
-
-
-
-
-
+app.use(userController);
 
 
 app.listen(port, () => {
---
+++ controllers/userController.js
@@ -0,0 +1,123 @@
+const express= require('express');
+
+const router =  new express.Router();
+const User = require("../models/user");
+
+
+
+router.get('/', (req, res) => {
+
+    res.send("Api's Are Working");
+
+});
+
+router.post('/saveUser', async (req, res) => {
+
+    try {
+
+        const user = new User(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+        console.log(createUser);
+
+
+    } catch (e) { 
+
+        res.status(400).send("error is occuring " + e);
+
+    }
+});
+
+
+router.get('/getUser', async(req,res)=>{
+
+    try{
+
+        const data = await User.find();
+        res.status(201).send(data);
+
+    }catch(e){
+
+        res.status(400).send("error" + e);
+
+    }
+
+});
+
+
+router.get('/getUserById/:id', async(req,res)=>{
+
+
+    try{
+        const _id = req.params.id;
+        const data = await User.findById(_id);
+        res.send(data);
+
+    } catch(e){
+
+        res.status(400).send("error" + e);
+
+    }
+
+});
+
+
+router.get('/getUserByname/:name', async(req,res)=>{
+
+    try{
+
+        const name= req.params.name;
+        const data =await User.findOne({name:name});
+        res.status(201).send(data);
+
+        
+        if(data !=null){
+
+            res.status(201).send(data);
+
+        }
+
+        else{
+
+            res.status(201).send(name + "not found in database");
+
+        }
+    }catch (e){
+
+        res.status(201).send("error" + e);
+
+    }
+
+});
+
+
+router.get('/getUserByEmail/:email', async(req,res)=>{
+
+    try{
+
+        const email= req.params.email;
+        const data =await User.findOne({email:email});
+       
+
+        if(data !=null){
+
+            res.status(201).send(data);
+
+        }
+
+        else{
+
+            res.status(201).send(email + "not found in database");
+
+        }
+    }catch (e){
+
+        res.status(201).send("error" + e);
+
+    }
+
+});
+
+
+module.exports=router;
+
